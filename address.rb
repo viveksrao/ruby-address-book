dd6419afa2bc3ff3b36fb7c76dfd8ceb59316e4e
Vivekanand Rao <vivek.rao@teachscape.com>
2015-07-03 18:56:30 -0700

    Added methods to save contacts in File.

--- address.rb
+++ address.rb
@@ -20,28 +20,3 @@ class Address
     address
   end
 end
-
-jason_home = Address.new
-jason_home.kind = "Home"
-jason_home.street_1 = "4709 California Street"
-jason_home.city = "San Francisco"
-jason_home.state = "CA"
-jason_home.postal_code = "94118"
-
-john_home = Address.new
-john_home.kind = "Home"
-john_home.street_1 = "310 Arballo Drive"
-john_home.street_2 = "Apt MH"
-john_home.city = "San Francisco"
-john_home.state = "CA"
-john_home.postal_code = "94132"
-
-# puts jason_home.to_s('short')
-# puts "\n"
-# puts jason_home.to_s('long')
-#
-# puts "\n"
-# puts john_home.to_s('short')
-# puts "\n"
-# puts john_home.to_s('long')
-# puts "\n"
--- address_book.rb
+++ address_book.rb
@@ -1,10 +1,24 @@
 require "./contact"
+require "yaml"
 
 class AddressBook
   attr_reader :contacts
 
   def initialize
     @contacts = []
+    open()
+  end
+
+  def open
+    if File.exist?("contacts.yaml")
+      @contacts = YAML.load_file("contacts.yaml")
+    end
+  end
+
+  def save
+    File.open("contacts.yaml","w") do |file|
+      file.write(contacts.to_yaml)
+    end
   end
 
   def display_menu
@@ -28,6 +42,7 @@ class AddressBook
         find_by_phone_number(search)
         find_by_address(search)
       when 'e'
+        save()
         break
       end
     end
@@ -137,43 +152,3 @@ end
 
 address_book = AddressBook.new
 address_book.display_menu
-
-# jason = Contact.new
-# jason.first_name = "Jason"
-# jason.last_name = "Seifer"
-# jason.add_phone_number("Home","123-456-7890")
-# jason.add_phone_number("Work","789-123-4560")
-# jason.add_phone_number("Cell","456-789-1230")
-# jason.add_address("Home","4709 California Street","","San Francisco","CA","94118")
-# jason.add_address("Work","655 Montgomery St","8th Floor","San Francisco","CA","94111")
-#
-# address_book.contacts.push(jason)
-
-# john = Contact.new
-# john.first_name = "John"
-# john.middle_name = "Christopher"
-# john.last_name = "Lobo"
-# john.add_phone_number("Home","111-222-3333")
-# john.add_phone_number("Work","222-333-1111")
-# john.add_phone_number("Cell","333-111-2222")
-# john.add_address("Home","310 Arballo Drive","Apt MH","San Francisco","CA","94132")
-# john.add_address("Work","345 Marine Drive","29th Floor","San Jose","CA","94555")
-#
-# address_book.contacts.push(john)
-
-# tom = Contact.new
-# tom.first_name = "Tom"
-# tom.middle_name = "Brandon"
-# tom.last_name = "Crawford"
-# tom.add_phone_number("Home","444-555-6666")
-# tom.add_phone_number("Work","555-666-4444")
-# tom.add_phone_number("Cell","666-444-5555")
-# tom.add_address("Home","123 Fremont Street","Apt 101","San Bruno","CA","94102")
-# tom.add_address("Work","1505 Milpitas Street","7th Floor","Milpitas","CA","94333")
-#
-# address_book.contacts.push(tom)
-
-# address_book.print_contact_list
-# address_book.find_by_name("Lobo")
-# address_book.find_by_phone_number("123-456-7890")
-# address_book.find_by_address("MH")
